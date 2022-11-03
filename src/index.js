f126cadf3dc1242ab3a464a5d3e9151b79cb1400
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-03 10:09:14 -0500

    complete all deliverables and some bonus

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// your code here
+
+const submitForm = document.querySelector('#create-task-form')
+let priority = 0
+
+submitForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const textField = e.target.querySelector('#new-task-description')
+  const inputTask = textField.value
+
+  let li = document.createElement('li')
+  li.innerText = inputTask
+  li.appendChild(makeDeleteButton(li))
+  setPriority(li)
+
+  const todoList = document.querySelector('#list')
+  todoList.appendChild(li)
+
+  textField.value = ''
+})
+
+function makeDeleteButton(listItem) {
+
+  const deleteButton = document.createElement('button')
+  deleteButton.innerText = 'Delete Task'
+
+  deleteButton.addEventListener('click', () => listItem.parentElement.removeChild(listItem))
+
+  return deleteButton
+}
+
+const priorityDropDown = document.querySelector('select')
+priorityDropDown.addEventListener('change', (e) => {
+  priority = e.target.selectedIndex
+  console.log(priority)
+})
+
+function setPriority(listItem) {
+  let color = ''
+
+  switch(priority) {
+    case 0:
+      listItem.classList.add('high-priority')
+      break;
+    case 1:
+      listItem.classList.add('medium-priority')
+      break;
+    case 2:
+      listItem.classList.add('low-priority')
+  }
+}
